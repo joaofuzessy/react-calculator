3df86a9044920e2aa6d6e5fc2dca1bc2cd54117a
João Füzessy <joao.fuzessy@gmail.com>
2019-11-05 17:59:26 -0300

    Updates

---
+++ precache-manifest.64669fe930d6bdbbf6764c73bff79130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bca02be4b389f38ece9282f82e13dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4b23fa4881253fb5faf",
+    "url": "/static/css/main.bcc8d27b.chunk.css"
+  },
+  {
+    "revision": "64ba68e65be4ec852a84",
+    "url": "/static/js/2.09a186fe.chunk.js"
+  },
+  {
+    "revision": "d4b23fa4881253fb5faf",
+    "url": "/static/js/main.cee5e44f.chunk.js"
+  },
+  {
+    "revision": "d2a7e81dafa2ca342df3",
+    "url": "/static/js/runtime-main.1ee0a06d.js"
+  },
+  {
+    "revision": "9d1044ccdbba0efa9a2bfc719a446702",
+    "url": "/static/media/RobotoMono-Light.9d1044cc.ttf"
+  }
+]);

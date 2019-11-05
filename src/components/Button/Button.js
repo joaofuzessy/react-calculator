53a47eb655e025521b9dd81d202359d250307fe4
João Füzessy <joao.fuzessy@gmail.com>
2019-11-05 15:26:14 -0300

    View closed - buttons color and sizes

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,4 +2,12 @@ import React from 'react'
 import './Button.scss'
 
 export default props =>
-<button className='button'>{props.label}</button>
+<button 
+        onClick={e => props.click && props.click(props.label)}
+        className={`
+                    ${props.operation ? 'operation' : ''}
+                    ${props.double ? 'double' : ''}
+                    ${props.triple ? 'triple' : ''}
+                  `}>
+    {props.label}
+</button>
